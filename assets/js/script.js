052e221111deab837ff4cd61b5b0ba7df16e3b35
shelbykohring <71856110+kohringsw@users.noreply.github.com>
2020-11-12 16:59:26 -0600

    fetch weather data for current weather and forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,58 @@ var cityFormEl = document.querySelector("#city-form");
 var savedCitiesEl = document.querySelector("#history");
 var clearHistoryBtn = document.querySelector("#clear-history");
 
+var uvIndex = function (lat, lon) {
+  //api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+  http: var apiUrl =
+    "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=7682a77c64471c5fd05aba694f7c40b8";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+      document.getElementById("uv-index").textContent =
+        "UV Index: " + data.value;
+    });
+  });
+};
+
+var getFiveDay = function (city) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial" +
+    "&appid=7682a77c64471c5fd05aba694f7c40b8";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+      var forecast = data.list;
+      var htmlData = [];
+      for (let i = 0; i < forecast.length; i = i + 8) {
+        var data = `
+        <p> ${forecast[i].dt_txt}</p>
+        <h6>Temperature: ${forecast[i].main.temp}</h6>
+        <h6>Humidity: ${forecast[i].main.humidity}</h6>
+        <h6>Wind Speed: ${forecast[i].wind.speed}</h6>
+        <img src="https://openweathermap.org/img/wn/${forecast[i].weather[0].icon}@2x.png"`;
+        htmlData.push(data);
+      }
+
+      document.getElementById("day-1").innerHTML = htmlData[0];
+      document.getElementById("day-2").innerHTML = htmlData[1];
+      document.getElementById("day-3").innerHTML = htmlData[2];
+      document.getElementById("day-4").innerHTML = htmlData[3];
+      document.getElementById("day-5").innerHTML = htmlData[4];
+      // document.getElementById("weather-icon").setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+    });
+  });
+};
+
 var getCityWeather = function (city) {
   // format the github api url
   var apiUrl =
@@ -16,6 +68,26 @@ var getCityWeather = function (city) {
     .then(function (response) {
       response.json().then(function (data) {
         console.log(data);
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        uvIndex(lat, lon);
+        document.getElementById("city-name").textContent = city;
+        document.getElementById("temperature").textContent =
+          "Temperature: " + data.main.temp;
+        document.getElementById("humidity").textContent =
+          "Humidity: " + data.main.humidity;
+        document.getElementById("wind-speed").textContent =
+          "Wind Speed: " + data.wind.speed;
+        document.getElementById("description").textContent =
+          data.weather[0].description;
+        document
+          .getElementById("weather-icon")
+          .setAttribute(
+            "src",
+            "https://openweathermap.org/img/wn/" +
+              data.weather[0].icon +
+              "@2x.png"
+          );
       });
     })
     .then(function (response) {
@@ -28,9 +100,10 @@ var cityInputHandler = function (event) {
   event.preventDefault();
   // get value from input element
   var cityName = cityInputEl.value.trim();
-
+  console.log(cityName);
   if (cityName) {
     getCityWeather(cityName);
+    getFiveDay(cityName);
     //clear cityInputEl.value
     cityInputEl.value = "";
   } else {
@@ -60,9 +133,10 @@ var saveCity = function (newCity) {
 // display the list of searched cities
 var displayCities = function () {
   var history = document.querySelector("#history");
+  history.innerHTML = "";
   for (var i = 0; i < localStorage.length; i++) {
     var city = localStorage.getItem("cities" + i);
-    var historyEl = document.createElement("a");
+    var historyEl = document.createElement("button");
     historyEl.classList = "form-control mb-2";
     // historyEl.setAttribute = ("href", getCityWeather);
     historyEl.innerHTML = city;
@@ -73,7 +147,8 @@ displayCities();
 
 var clearHistory = function () {
   localStorage.clear();
+  history.innerHTML = "";
   displayCities(history);
-}
+};
 
 clearHistoryBtn.addEventListener("click", clearHistory);
